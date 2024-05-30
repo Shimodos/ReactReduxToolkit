589a99b94d0ce3a3adf75f6646f8ddec67573441
Aleksey Demchenko <ad@pesochin.net>
2024-05-30 22:11:53 +0300

    3.16: First component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <>
       <h1>Title</h1>
       <p>Project</p>
+      <Button />
     </>
   );
 }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,5 @@
+function Button() {
+  return <button>Save</button>;
+}
+
+export default Button;
