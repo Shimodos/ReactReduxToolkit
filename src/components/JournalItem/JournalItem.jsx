25488137f4005248df67f9f41d1ee901c0ce234f
Aleksey Demchenko <ad@pesochin.net>
2024-05-30 23:20:37 +0300

    3.19: Dinami data

---
+++ src/components/JournalItem/JournalItem.jsx
@@ -0,0 +1,19 @@
+import './JournalItem.css';
+
+function JournalItem() {
+  const title = ' Подготовка к обновлению курсов по React';
+  const date = new Date().toLocaleDateString();
+  const text = 'Все курсы по React будут обновлены до последней версии библиотеки.';
+
+  return (
+    <div className="journal-item">
+      <h2 className="journal-item__headr">{title}</h2>
+      <h2 className="journal-item__body">
+        <div className="journal-item__data">{date}</div>
+        <div className="journal-item__text">{text}</div>
+      </h2>
+    </div>
+  );
+}
+
+export default JournalItem;
