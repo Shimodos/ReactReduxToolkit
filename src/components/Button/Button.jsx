a9e2cf959bee02c1cb5438a247369a33f70af71c
Aleksey Demchenko <ad@pesochin.net>
2024-05-31 19:33:38 +0300

    4.25: useState

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,19 @@
 import './Button.css';
+import { useState } from 'react';
 
 function Button() {
+  //useState
+  const [text, setText] = useState('Save');
+
+  console.log('Rerendered');
   const handleClick = () => {
-    console.log('Hello');
+    setText((t) => (t === 'Save' ? 'Saved' : 'Save'));
+    console.log(text);
   };
 
   return (
     <button onClick={handleClick} className="button accent">
-      Save
+      {text}
     </button>
   );
 }
