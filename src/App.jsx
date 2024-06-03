c60304e6cd9ee35f6be0736106864c3b0d01c2b4
Aleksey Demchenko <ad@pesochin.net>
2024-06-03 19:24:14 +0300

    4.37: Display by condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react';
 import './App.css';
-// import Button from './components/Button/Button';
-import CardButton from './components/CardButton/CardButton';
-import JournalItem from './components/JournalItem/JournalItem';
 import Body from './layout/Dody/Body';
 import LeftPanel from './layout/LeftPanel/LeftPanel';
 import Header from './components/Header/Header';
@@ -11,24 +8,24 @@ import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalForm from './components/JournalForm/JournalForm';
 
 const INITIAL_DATA = [
-  {
-    id: 1,
-    title: ' Подготовка к обновлению курсов по React',
-    date: new Date(),
-    text: 'Все курсы по React будут обновлены до последней версии библиотеки.',
-  },
-  {
-    id: 2,
-    title: ' iOS 15: первый взгляд на новую операционную систему Apple',
-    date: new Date(),
-    text: 'Apple представила новую версию операционной системы iOS 15 на конференции WWDC 2021.',
-  },
-  {
-    id: 3,
-    title: 'vivo представила новый смартфон X60t Pro+ с процессором Snapdragon 888',
-    date: new Date(),
-    text: 'Компания vivo представила новый флагманский смартфон X60t Pro+ с процессором Snapdragon 888 и тройной камерой.',
-  },
+  // {
+  //   id: 1,
+  //   title: ' Подготовка к обновлению курсов по React',
+  //   date: new Date(),
+  //   text: 'Все курсы по React будут обновлены до последней версии библиотеки.',
+  // },
+  // {
+  //   id: 2,
+  //   title: ' iOS 15: первый взгляд на новую операционную систему Apple',
+  //   date: new Date(),
+  //   text: 'Apple представила новую версию операционной системы iOS 15 на конференции WWDC 2021.',
+  // },
+  // {
+  //   id: 3,
+  //   title: 'vivo представила новый смартфон X60t Pro+ с процессором Snapdragon 888',
+  //   date: new Date(),
+  //   text: 'Компания vivo представила новый флагманский смартфон X60t Pro+ с процессором Snapdragon 888 и тройной камерой.',
+  // },
 ];
 
 function App() {
@@ -46,26 +43,12 @@ function App() {
     ]);
   };
 
-  const sortItems = (a, b) => {
-    if (a.date > b.date) {
-      return 1;
-    } else {
-      return -1;
-    }
-  };
-
   return (
     <div className="app">
       <LeftPanel>
         <Header />
         <JournalAddButton />
-        <JournalList>
-          {items.sort(sortItems).map((item) => (
-            <CardButton key={item.id}>
-              <JournalItem title={item.title} date={item.date} text={item.text} />
-            </CardButton>
-          ))}
-        </JournalList>
+        <JournalList items={items} />
       </LeftPanel>
       <Body>
         <JournalForm onSubmit={addJournalItem} />
--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,7 +1,29 @@
 import './JournalList.css';
+import CardButton from '../CardButton/CardButton';
+import JournalItem from '../JournalItem/JournalItem';
 
-function JournalList({ children }) {
-  return <div className="journa-List">{children}</div>;
+function JournalList({ items }) {
+  if (items.length === 0) {
+    return <p>No entries, add new entries</p>;
+  }
+
+  const sortItems = (a, b) => {
+    if (a.date > b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  };
+
+  return (
+    <>
+      {items.sort(sortItems).map((item) => (
+        <CardButton key={item.id}>
+          <JournalItem title={item.title} date={item.date} text={item.text} />
+        </CardButton>
+      ))}
+    </>
+  );
 }
 
 export default JournalList;
