d734d8922365d55618c8f064abe561a5b8df2c21
Aleksey Demchenko <ad@pesochin.net>
2024-06-03 18:24:58 +0300

    4.36: Key

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,19 @@ import JournalForm from './components/JournalForm/JournalForm';
 
 const INITIAL_DATA = [
   {
+    id: 1,
     title: ' Подготовка к обновлению курсов по React',
     date: new Date(),
     text: 'Все курсы по React будут обновлены до последней версии библиотеки.',
   },
   {
+    id: 2,
     title: ' iOS 15: первый взгляд на новую операционную систему Apple',
     date: new Date(),
     text: 'Apple представила новую версию операционной системы iOS 15 на конференции WWDC 2021.',
   },
   {
+    id: 3,
     title: 'vivo представила новый смартфон X60t Pro+ с процессором Snapdragon 888',
     date: new Date(),
     text: 'Компания vivo представила новый флагманский смартфон X60t Pro+ с процессором Snapdragon 888 и тройной камерой.',
@@ -38,8 +41,17 @@ function App() {
         title: item.title,
         date: new Date(item.date),
         text: item.text,
+        id: Math.max(...oldItem.map((item) => item.id)) + 1,
       },
     ]);
+  };
+
+  const sortItems = (a, b) => {
+    if (a.date > b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
   };
 
   return (
@@ -48,8 +60,8 @@ function App() {
         <Header />
         <JournalAddButton />
         <JournalList>
-          {items.map((item) => (
-            <CardButton>
+          {items.sort(sortItems).map((item) => (
+            <CardButton key={item.id}>
               <JournalItem title={item.title} date={item.date} text={item.text} />
             </CardButton>
           ))}
