4ebf7fa705344c8c871da78790fa868ecbdf19e3
Aleksey Demchenko <ad@pesochin.net>
2024-06-07 20:54:17 +0300

    5.45: useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Body from './layout/Dody/Body';
 import LeftPanel from './layout/LeftPanel/LeftPanel';
@@ -7,29 +7,15 @@ import JournalList from './components/JournalList/JournalList';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalForm from './components/JournalForm/JournalForm';
 
-const INITIAL_DATA = [
-  // {
-  //   id: 1,
-  //   title: ' Подготовка к обновлению курсов по React',
-  //   date: new Date(),
-  //   text: 'Все курсы по React будут обновлены до последней версии библиотеки.',
-  // },
-  // {
-  //   id: 2,
-  //   title: ' iOS 15: первый взгляд на новую операционную систему Apple',
-  //   date: new Date(),
-  //   text: 'Apple представила новую версию операционной системы iOS 15 на конференции WWDC 2021.',
-  // },
-  // {
-  //   id: 3,
-  //   title: 'vivo представила новый смартфон X60t Pro+ с процессором Snapdragon 888',
-  //   date: new Date(),
-  //   text: 'Компания vivo представила новый флагманский смартфон X60t Pro+ с процессором Snapdragon 888 и тройной камерой.',
-  // },
-];
-
 function App() {
-  const [items, setItems] = useState(INITIAL_DATA);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const date = JSON.parse(localStorage.getItem('date'));
+    if (date) {
+      setItems(date.map((item) => ({ ...item, date: new Date(item.date) })));
+    }
+  }, []);
 
   const addJournalItem = (item) => {
     setItems((oldItem) => [
