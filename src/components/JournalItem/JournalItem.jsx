419d29d72df61e4a543ba3473df2d7e0f0b93b33
Aleksey Demchenko <ad@pesochin.net>
2024-06-12 20:11:42 +0300

    7.52: Calear Form Fix Data format

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,49 +1,22 @@
-// import './JournalItem.css';
-
-// function JournalItem({ title, date, text }) {
-//   // const formatedDate = new Intl.DateTimeFormat('un-US').format(date);
-//   const formatedDate = new Intl.DateTimeFormat('uk-UA').format(date);
-
-//   return (
-//     <>
-//       <h2 className="journal-item__headr">{title}</h2>
-//       <h2 className="journal-item__body">
-//         <div className="journal-item__data">{formatedDate}</div>
-//         <div className="journal-item__text">{text}</div>
-//       </h2>
-//     </>
-//   );
-// }
-
-// export default JournalItem;
-
 import './JournalItem.css';
 
-function JournalItem({ title, date, text }) {
-  let formattedDate;
-
-  try {
-    // Ensure date is a valid Date object or a valid date string
-    const validDate = new Date(date);
-    if (!isNaN(validDate.getTime())) {
-      formattedDate = new Intl.DateTimeFormat('uk-UA').format(validDate);
-    } else {
-      throw new Error('Invalid date');
-    }
-  } catch (error) {
-    console.error('Error formatting date:', error);
-    formattedDate = 'Invalid date';
+function JournalItem({ title, text, date }) {
+  // Check if date is a valid Date object
+  if (!(date instanceof Date && !isNaN(date))) {
+    // Throw an error or use a default value
+    date = new Date(); // Using current date as default
   }
+
+  // formatedDate
+  const formatedDate = new Intl.DateTimeFormat('uk-UA').format(date);
 
   return (
     <>
-      <h2 className="journal-item__header">{title}</h2> {/* Исправлена опечатка */}
-      <div className="journal-item__body">
-        {' '}
-        {/* Исправлена структура HTML */}
-        <div className="journal-item__date">{formattedDate}</div>
+      <h2 className="journal-item__header">{title}</h2>
+      <h2 className="journal-item__body">
+        <div className="journal-item__date">{formatedDate}</div>
         <div className="journal-item__text">{text}</div>
-      </div>
+      </h2>
     </>
   );
 }
