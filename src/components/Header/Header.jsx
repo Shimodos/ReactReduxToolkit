cd79b241bfdb8ba6ecddeccda769ecd5131a4d46
Aleksey Demchenko <ad@pesochin.net>
2024-06-14 18:56:06 +0300

    9.68: useCallback

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import SelectUser from '../SelectUser/SelectUser';
 import Button from '../Button/Button';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import Logo from '../Logo/Logo';
 
 const logos = ['/Logo.svg', '/vite.svg'];
@@ -9,9 +9,10 @@ function Header() {
   const [logo, setLogo] = useState(0);
   console.log('Header rendere');
 
-  const toggleLogo = () => {
+  const toggleLogo = useCallback(() => {
     setLogo((prev) => (prev === 0 ? 1 : 0));
-  };
+  }, []);
+
   return (
     <>
       <Logo image={logos[logo]} />
